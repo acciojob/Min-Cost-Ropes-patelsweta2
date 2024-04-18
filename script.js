4515e83c2c2aa45748cd794f7149d10316aad98d
patelsweta2 <80040744+patelsweta2@users.noreply.github.com>
2024-04-18 15:07:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 function mincost(arr)
 { 
 //write your code here
+	let totalCost = 0;
+	while(arr.length > 1) {
+		arr.sort((a,b) => a-b);
+		const min1 = arr.shift();
+		const min2 = arr.shift();
+
+		const combinedLength = min1 + min2;
+		totalCost+=combinedLength;
+		arr.push(combinedLength);
+	}
 // return the min cost
+	return totalCost;
   
 }
 
